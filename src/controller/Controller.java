35a6601c6a87bf5434a46f094307d52411763292
Muhammad Usman Ashfaq <f200115@cfd.nu.edu.pk>
2022-12-07 17:53:27 +0500

    Revert "Added functionality Modification and Removal of word from Word table of DB in buttons"
    This reverts commit e9c62d93065df5ef6b284a5032915d6965f4bac7.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -4,10 +4,9 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
 import javax.swing.table.TableModel;
+
 import model.*;
-import tranferObjects.TransferObject;
 import view.*;
 
 /**
@@ -16,15 +15,6 @@ import view.*;
  *
  */
 public class Controller {
-	private TransferObject obj = new TransferObject();
-
-	public TransferObject getObj() {
-		return obj;
-	}
-
-	public void setObj(TransferObject obj) {
-		this.obj = obj;
-	}
 
 	private Model model;
 	private View view;
@@ -59,11 +49,10 @@ public class Controller {
 		view.dispose();
 		SecondFrame sf = new SecondFrame(model.getListOfWords());
 		sf.setVisible(true);
-		sf.gettTextField().setText("");
 		sf.getTable().addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				sf.setIndexRowSelected(sf.getTable().getSelectedRow());
+				sf.setIndexRowSelected(sf.getTable().getSelectedRow()) ;
 				TableModel model = sf.getTable().getModel();
 //				int id = Integer.parseInt(model.getValueAt(indexRowSelected, 0).toString());
 //				int frequency = Integer.parseInt(model.getValueAt(indexRowSelected, 1).toString());
@@ -75,45 +64,17 @@ public class Controller {
 		});
 		sf.getBtnNewButton().addActionListener(e -> {
 			sf.getTableModel();
-			if (sf.getIndexRowSelected() == -1) {
-				JOptionPane.showMessageDialog(null, "جناب ٹیبل سے لفظ منتخب کریں۔");
-				sf.gettTextField().setText("");
-				return;
-			}
 			int id = Integer.parseInt(sf.getTableModel().getValueAt(sf.getIndexRowSelected(), 0).toString());
 			int frequency = Integer.parseInt(sf.getTableModel().getValueAt(sf.getIndexRowSelected(), 1).toString());
-			if (!(sf.gettTextField().getText().equals(""))) {
-
-				String word = sf.getTextField();
-				int i = word.indexOf(' ');
-				word = word.substring(0, i);
-				obj.setWordId(id);
-				obj.setWord(word);
-				System.out.println(
-						"Updated Id is : " + id + " frequency is : " + frequency + " word is : " + obj.getWord());
-				model.getValueofUpdatedWord(obj);
-				sf.gettTextField().setText("");
-			} else {
-				JOptionPane.showMessageDialog(null, "براہ کرم پہلے ایک لفظ کا انتخاب کریں");
-			}
+			String word = sf.getTextField();
+			int i = word.indexOf(' ');
+			word = word.substring(0, i);
+			System.out.println("Updated Id is : " + id + " frequency is : " + frequency + " word is : " + word);
 		});
 		sf.getBtnNewButton_1().addActionListener(e -> {
 			sf.getTableModel();
-			if (sf.getIndexRowSelected() == -1) {
-				JOptionPane.showMessageDialog(null, "جناب ٹیبل سے لفظ منتخب کریں۔");
-				sf.gettTextField().setText("");
-				return;
-			}
-			if (!(sf.gettTextField().getText().equals(""))) {
-				int id = Integer.parseInt(sf.getTableModel().getValueAt(sf.getIndexRowSelected(), 0).toString());
-				obj.setWordId(id);
-				System.out.println("Deleting Id is : " + obj.getWordId());
-				model.deleteWordIdForDb(obj);
-				sf.gettTextField().setText("");
-			} else {
-				JOptionPane.showMessageDialog(null, "براہ کرم پہلے ایک لفظ کا انتخاب کریں");
-			}
+			int id = Integer.parseInt(sf.getTableModel().getValueAt(sf.getIndexRowSelected(), 0).toString());
+			System.out.println("Deleting Id is : " + id );
 		});
 	}
-
 }
